42153f1a4d171806aff5c449c16ca522cdee19f4
NFRamz <133985045+NFRamz@users.noreply.github.com>
2026-03-18 00:02:39 +0700

    Update backend.js

--- backend.js
+++ backend.js
@@ -162,7 +162,7 @@ setInterval(() => {
   io.emit('updatePlayers', backEndPlayers)
 }, 15)
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`)
 })
 
